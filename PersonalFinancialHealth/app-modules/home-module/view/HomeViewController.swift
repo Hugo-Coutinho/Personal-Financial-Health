051df67ce7b7d1b322bc717efbf17392e8e3481d
Rogerinho <hugocoutinho@brq.com>
2019-07-08 17:18:05 -0300

    home layout finalizado

--- PersonalFinancialHealth/app-modules/home-module/view/HomeViewController.swift
+++ PersonalFinancialHealth/app-modules/home-module/view/HomeViewController.swift
@@ -2,8 +2,63 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    
+    // MARK: - OUTLETS -
+    @IBOutlet weak var availableFundsView: UIView!
+    @IBOutlet weak var myExpensesView: UIView!
+    @IBOutlet weak var netSalaryView: UIView!
+    @IBOutlet weak var historicalView: UIView!
+}
 
+// MARK: - LIFE CYCLE -
+extension HomeViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupView()
+    }
+}
+
+// MARK: - AUX METHODS -
+extension HomeViewController {
+    private func setupView() {
+        self.addGesture(view: self.availableFundsView)
+        self.addGesture(view: self.myExpensesView)
+        self.addGesture(view: self.netSalaryView)
+        self.addGesture(view: self.historicalView)
+        self.addTag()
+    }
+
+    private func addGesture(view: UIView) {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.tapToNewPage(_:)))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc private func tapToNewPage(_ sender: Any) {
+    guard let tapGesture = sender as? UITapGestureRecognizer,
+    let attachedView = tapGesture.view else { return }
+    redirectToNewPageBy(index: attachedView.tag)
+    }
+    
+    private func redirectToNewPageBy(index: Int) {
+        switch index {
+        case 0:
+            print("availableFunds")
+        case 1:
+            print("expenses")
+        case 2:
+            print("net salary")
+        case 3:
+            print("historical")
+        default:
+            break
+        }
+    }
+    
+    private func addTag() {
+        self.availableFundsView.tag = 0
+        self.myExpensesView.tag = 1
+        self.netSalaryView.tag = 2
+        self.historicalView.tag = 3
     }
 }
